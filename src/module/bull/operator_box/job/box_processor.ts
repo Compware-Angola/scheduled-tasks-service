8533d8d09d1ddbe7680647957b3272b738902c20
Domingos Canhanga <canhanga96@gmail.com>
2026-05-29 14:12:36 +0100

    feat: implement operator box blocking logic and clean up processor job handler

--- src/module/bull/operator_box/job/box_processor.ts
+++ src/module/bull/operator_box/job/box_processor.ts
@@ -4,9 +4,7 @@ import { Job } from 'bullmq';
 import { OperatorBoxService } from '../service/operator_box_service.service';
 @Processor('operator_box')
 export class BoxProcessor extends WorkerHost {
-    private readonly logger = new Logger(BoxProcessor.name);
     constructor(
-
         private readonly operatorBoxService: OperatorBoxService,
     ) {
         super();
@@ -16,15 +14,9 @@ export class BoxProcessor extends WorkerHost {
         console.log(`Processing job ${job.id} of type ${job.name}`);
         if (job.name === 'processOperatorBox') {
             const { codigoUtilizador } = job.data;
-
-
             await this.operatorBoxService.processOperatorBox(codigoUtilizador);
-            console.log(`Job ${job.id} completed successfully.`);
-            return { success: true };
         }
-
-        console.log(`Job ${job.id} has an unknown type: ${job.name}`);
-        return { success: false, "message": 'Unknown job type' };
+        return { success: true };
 
     }
     @OnWorkerEvent('completed')
--- src/module/bull/operator_box/service/operator_box_service.service.ts
+++ src/module/bull/operator_box/service/operator_box_service.service.ts
@@ -1,16 +1,27 @@
 import { Logger } from "@nestjs/common";
 import { DataSource } from "typeorm";
+import { Injectable } from "@nestjs/common";
 
-
+@Injectable()
 export class OperatorBoxService {
     private readonly logger = new Logger(OperatorBoxService.name);
-
     constructor(private readonly dataSource: DataSource) { }
     async processOperatorBox(codigoUtilizador: number) {
-        console.log(`Processing operator box for operator: ${codigoUtilizador}`);
-
         try {
-            //VERIFICAR SE È OPERADOR  E PROCESSGUIR .....
+            const result = await this.dataSource.query(
+    `
+    UPDATE FK2_TB_CAIXAS
+    SET BLOQUEIO = :blocked
+    WHERE OPERADOR_ID = :operatorId
+      AND DELETED_AT IS NULL
+    `,
+    {
+      blocked: 'S',
+      operatorId:codigoUtilizador,
+    } as any,
+  );
+
+  return result;
 
         }
         catch (err) {
